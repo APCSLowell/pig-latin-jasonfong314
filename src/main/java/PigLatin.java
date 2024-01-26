991bb3c27615481f913471a0fd7cc78b1f5195a0
J <143027556+jasonfong314@users.noreply.github.com>
2024-01-26 12:44:25 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,3 +1,4 @@
+int storeI = 0;
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
@@ -28,21 +29,46 @@ public class PigLatin {
 	    }
     }
     public int findFirstVowel(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0.
-        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    // your code goes here
-        return -1;
+      for (int i = 0; i<sWord.length(); i++) {
+    if (sWord.substring(i, i+1).equals ("a")) {
+      storeI = i;
+      return i;
     }
-
+    if (sWord.substring(i, i+1).equals ("e")) {
+      storeI = i;
+      return i;
+    }
+    if (sWord.substring(i, i+1).equals ("i")) {
+      storeI = i;
+      return i;
+    }
+    if (sWord.substring(i, i+1).equals ("o")) {
+      storeI = i;
+      return i;
+    }
+    if (sWord.substring(i, i+1).equals ("u")) {
+      storeI = i;
+      return i;
+    }
+  }
+  return -1;
+}
     public String pigLatin(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0
-        //postcondition: returns the pig latin equivalent of sWord
-        // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
-    }
-}//end PigLatin class
+if (sWord.charAt(0)=='q' && sWord.charAt(1)=='u') {
+    return  sWord.substring (2, sWord.length()) + sWord.substring (0, 2)+ "ay";
+  }
+  //       if (findFirstVowel(sWord) != -1) {
+
+
+  if (findFirstVowel(sWord) == 0)
+  {
+    return sWord + "way";
+  }
+  if (findFirstVowel(sWord) == -1)
+  {
+    return sWord + "ay";
+  } else
+  {
+    return (sWord.substring (storeI) + sWord.substring (0, storeI)+ "ay");
+  }
+}
